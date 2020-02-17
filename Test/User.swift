3a5335b21c578dfa00a763234b7a61b0258f1c49
sainath <g.sainath2312@gmail.com>
2020-02-17 12:36:05 +0530

    first commit

---
+++ Test/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Test
+//
+//  Created by Sainath Gajavada on 17/02/20.
+//  Copyright © 2020 Sainath Gajavada. All rights reserved.
+//
+
+import Foundation
---
+++ Test/Utility.swift
@@ -0,0 +1,9 @@
+//
+//  Utility.swift
+//  Test
+//
+//  Created by Sainath Gajavada on 17/02/20.
+//  Copyright © 2020 Sainath Gajavada. All rights reserved.
+//
+
+import Foundation
